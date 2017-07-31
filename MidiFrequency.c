4266af03e0cd2188c63712a78db32558cbe165bd
Roseburrow <thomas.brewer@protonmail.co.uk>
2017-07-31 19:15:45 +0100

    Added validation and some optimisation

--- MidiFrequency.c
+++ MidiFrequency.c
@@ -13,27 +13,33 @@ int main()
 
     //CALCULATE REQUIRED VALUES.
 
-    //This number comes from the fact that to raise something a semitone you
-    //must multiply it by this ratio. (I think.)
-    semitone_ratio = pow(2, 1/12.0); //Approx 1.0594631.
+    /*To raise something a semitone do the semitone ratio to the power of the
+      number of semitones you are raising by.*/
+    semitone_ratio = pow(2.0, 1.0/12.0); //Approx 1.0594631.
 
     /*Find middle C, this is 3 semitones above low A which is 220Hz.
-    EDIT: To elaborate this means find the frequency of middle C. The to the
-    of 3 part is becuase we are using a starting base of frequency^3.*/
-    c5 = 220.0 * pow(semitone_ratio, 3);
+      This is a nice round figure to use to find middle C.*/
+    c5 = 220.0 * pow(semitone_ratio, 3.0);
 
-    /*MIDI note 0 is C which is 5 octave below middle C. We need this frequency
-    as well to know the starting frequency we are multiplying to get the value
-    we want. 0.5 is an octave fall in terms of a power. The 5 means 5 octaves
-    below the given c5 note which is middle C. Ultimately this gets us the freq
-    of C (midi note 0 below middle C.*/
-    c0 = c5 * pow(0.5, 5);
-
-    printf("Please enter the MIDI note value you want the frequency of...\n");
-    scanf("%d", &midinote);
+    /*MIDI note 0 is C which is 5 octave below middle C. Since we know Middle C
+    this can easily be found. 0.5 is an octave fall in terms of a power. So
+    0.5^5 is a fall of 5 octaves.*/
+    c0 = c5 * pow(0.5, 5.0);
     
+    while(1)
+    {
+	printf("Please enter the MIDI note you want the frequency of: ");
+	scanf("%d", &midinote);
+	
+	if (midinote > 127 || midinote < 0)
+	{
+	    printf("\nPlease eneter a value between 0-127.\n\n");
+	}
+	else break;
+    }
+
     /*so at the end of it all this calculation equates to:
-    The frequency of midi note 0 * semitone_ratio to the power of the midi note
+    The frequency of midi note 0 * semitone_ratio^the midi note
     we want the frequency of.*/
 
     /*Ultimately:
